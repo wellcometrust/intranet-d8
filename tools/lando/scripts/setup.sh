b453812f34b49c0583b6de6e6794c69cf241be32
Scott Hooker <scott.hooker@gmail.com>
2019-05-13 11:50:03 +0100

    D8 standard config for articles.

--- tools/lando/scripts/setup.sh
+++ tools/lando/scripts/setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 #Add certificate
 #sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.lando/certs/lndo.site.pem
-lando destroy -y
-lando start
-lando db-import tools/lando/assets/database/db.sql --host migrationdb
+#lando destroy -y
+#lando start
+#lando db-import tools/lando/assets/database/db.sql --host migrationdb
 lando drush si trustnet --account-name=admin --account-pass=admin -y --existing-config
+lando drush en migrate_tools,migrate_file,tn_group_migration,tn_content_migration,tn_migration,migrate,migrate_drupal -y
+lando drush migrate:rollback tn_groups_groups
+lando drush migrate:import tn_groups_groups
+lando drush migrate:import tn_content_articles
+lando drush pm-uninstall migrate_tools,migrate_file,tn_group_migration,tn_content_migration,tn_migration,migrate,migrate_drupal -y
